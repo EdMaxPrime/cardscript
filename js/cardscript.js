c2c1d3ae5c29a33b243e34e4ec96270b17c228c0
Max Z <max.zlotskiy@gmail.com>
2017-04-01 23:55:00 -0400

    updated order to match 10 symbol

--- js/cardscript.js
+++ js/cardscript.js
@@ -115,7 +115,7 @@
         this.settings = merge({
             players: 1,
             rules: {a: "lol"},
-            order: {a:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, j:11, q:12, k:13}
+            order: {a:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, t:10, j:11, q:12, k:13}
         }, opts);
         var nextCardID = 0;
         this.createPile = function(list) {
